cd677c42347bd9b8502ad5be8df552863da3e6a4
Wiktor332 <wiktor.stanislaw.cwikla@gmail.com>
2024-01-15 16:58:07 +0100

    update

--- website_free_games/js_script/onclik_dropdown.js
+++ website_free_games/js_script/onclik_dropdown.js
@@ -1,4 +1,24 @@
+  
 const dropdowns = document.querySelectorAll('.dropdown');
+
+document.addEventListener('click', (event) => {
+  dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    const isClickInsideDropdown = dropdown.contains(event.target);
+
+    if (!isClickInsideDropdown) {
+      select.classList.remove('select-clicked');
+      menu.classList.remove('menu-open');
+      options.forEach(option => {
+        option.classList.remove('active');
+      });
+    }
+  });
+});
 
 dropdowns.forEach(dropdown => {
   const select = dropdown.querySelector('.select');
@@ -22,5 +42,4 @@ dropdowns.forEach(dropdown => {
     });
   });
 });
-  
 
